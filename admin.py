6ab5a966a0dbb4fe8542fb1d6f13f7e1522dc08f
agajo <agajo@users.noreply.github.com>
2017-10-16 16:03:45 +0900

    add Tsukkomi to Admin

--- admin.py
+++ admin.py
@@ -6,6 +6,10 @@ from .models import Odai
 
 admin.site.register(Odai)
 
-from . models import Answer
+from .models import Answer
 
 admin.site.register(Answer)
+
+from .models import Tsukkomi
+
+admin.site.register(Tsukkomi)
