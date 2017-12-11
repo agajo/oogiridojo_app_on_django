d8f3016cf53ef6a4b3c255d72e1d7cd0ee502d86
agajo <agajo@users.noreply.github.com>
2017-12-11 14:26:42 +0900

    admin modify.

--- admin.py
+++ admin.py
@@ -1,27 +1,22 @@
 from django.contrib import admin
+from .models import Odai, Answer, Tsukkomi, Judgement, Monkasei, Article, Practice
+from django.contrib.sessions.models import Session
 
 # Register your models here.
 
-from .models import Odai
 admin.site.register(Odai)
 
-from .models import Answer
-admin.site.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ['answer_text','monkasei']
 
-from .models import Tsukkomi
+admin.site.register(Answer, AnswerAdmin)
 admin.site.register(Tsukkomi)
-
-from .models import Judgement
 admin.site.register(Judgement)
-
-from django.contrib.sessions.models import Session
 admin.site.register(Session)
 
-from .models import Monkasei
-admin.site.register(Monkasei)
+class MonkaseiAdmin(admin.ModelAdmin):
+    list_display = ['name','ningenryoku']
 
-from .models import Article
+admin.site.register(Monkasei, MonkaseiAdmin)
 admin.site.register(Article)
-
-from .models import Practice
 admin.site.register(Practice)
